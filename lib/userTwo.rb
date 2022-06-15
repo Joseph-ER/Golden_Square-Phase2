d98e4dbfef0fce4be4e4e1c89e9ab23cf10b69a0
Joseph Edwards Rean <joseph.edwardsrean@outlook.com>
2022-06-15 10:45:26 +0100

    Completed phase 2 user two

--- lib/userTwo.rb
+++ lib/userTwo.rb
@@ -1,12 +1,16 @@
 def uppercaseStart(text)
   words = text.split(" ")
-  if words[0] != words[0].upcase
-    return "Remember to capitalize first word of a sentence!"
+  if words[0] == words[0].upcase
+    return false
+  else
+    return true
   end
 end
 
 def punctuationEnd(text)
-  if text[-1] != "." || text[-1] != "!" || text[-1] != "?"
-    return "Remember to end your sentence in punctuation!"
+  if text[-1] === "." || text[-1] == "!" || text[-1] == "?"
+    return true
+  else
+    return false
   end
 end
--- spec/userTwo_spec.rb
+++ spec/userTwo_spec.rb
@@ -4,14 +4,14 @@ RSpec.describe "uppercaseStart method" do
   context "given a string that is not capitalized on the first word" do
     it "returns capitalized reminder" do
       result = uppercaseStart("Bad sentence.")
-      expect(result).to eq "Remember to capitalize first word of a sentence!"
+      expect(result).to eq true
     end
   end
 
   context "given a string that does not end in punctuation" do
     it "returns a puncuation reminder" do
       result = punctuationEnd("Bad sentence.")
-      expect(result).to eq "Remember to end your sentence in punctuation!"
+      expect(result).to eq true💅
     end
   end
 end
